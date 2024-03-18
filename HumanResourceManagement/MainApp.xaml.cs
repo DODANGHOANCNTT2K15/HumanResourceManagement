285dc83535c6703b4f19072ca3c1a8e80f5d4483
DODANGHOANCNTT2K15 <dodanghoana12017@gmail.com>
2024-03-18 20:18:46 +0700

    Fix_Salary

--- HumanResourceManagement/MainApp.xaml.cs
+++ HumanResourceManagement/MainApp.xaml.cs
@@ -680,6 +680,8 @@ namespace HumanResourceManagement
             gridNhanh4.Visibility = Visibility.Hidden;
             gridNhanh5.Visibility = Visibility.Hidden;
             gridNhanh6.Visibility = Visibility.Visible;
+            gridNhanh6_LuongNV.Visibility = Visibility.Hidden;
+            gridNhanh6_ThongTinNV.Visibility = Visibility.Visible;
             gridNhanh7.Visibility = Visibility.Hidden;
             gridNhanh8_0.Visibility = Visibility.Hidden;
             gridNhanh8_1.Visibility = Visibility.Hidden;
@@ -692,6 +694,7 @@ namespace HumanResourceManagement
             gridNhanh10_3.Visibility = Visibility.Hidden;
 
             buttonEditOrderNV.Tag = maNV;
+            buttonLuongNV.Tag = maNV;
             string connString = @"Data Source=.\sqlexpress;Initial Catalog=QuanLyNhanSu;Integrated Security=True;Encrypt=False";
             string query = "SELECT * FROM tb_NHANVIEN WHERE MANV = @maNV";
             using (SqlConnection conn = new SqlConnection(connString))
@@ -1774,7 +1777,6 @@ namespace HumanResourceManagement
 
             foreach (ChucVu newChucVu in sqlValues)
             {
-                // Tạo một Border mới
                 Border border = new Border
                 {
                     Width = 711,
@@ -1786,7 +1788,6 @@ namespace HumanResourceManagement
                     VerticalAlignment = VerticalAlignment.Center
                 };
 
-                // Tạo một StackPanel mới
                 StackPanel stackPanel = new StackPanel
                 {
                     HorizontalAlignment = HorizontalAlignment.Left,
@@ -1796,12 +1797,8 @@ namespace HumanResourceManagement
                     Width = 991
                 };
 
-
-                // Tạo một Label mới và gán giá trị từ SQL vào Content
-
                 Label label = new Label
                 {
-
                     Content = newChucVu.MaCV,
                     Foreground = Brushes.White,
                     Width = 231,
@@ -1809,7 +1806,6 @@ namespace HumanResourceManagement
                     FontFamily = new FontFamily("Bahnschrift"),
                     FontSize = 14
                 };
-
 
                 Label label1 = new Label
                 {
@@ -1820,7 +1816,6 @@ namespace HumanResourceManagement
                     FontFamily = new FontFamily("Bahnschrift"),
                     FontSize = 14
                 };
-
 
                 Button button2 = new Button
                 {
@@ -2528,10 +2523,395 @@ namespace HumanResourceManagement
             }
         }
 
+        // Thông tin cá nhân nút chuyển sang thông tin cá nhân
         private void Button_Click_29(object sender, RoutedEventArgs e)
         {
             gridNhanh3_LuongCaNhan.Visibility = Visibility.Hidden;
             gridNhanh3_ThongTinCaNhan.Visibility = Visibility.Visible;
+        }
+        
+        //Thông tin nhân viên khác chuyển sang thông tin nhan viên
+        private void Button_Click_30(object sender, RoutedEventArgs e)
+        {
+            gridNhanh6_LuongNV.Visibility = Visibility.Hidden;
+            gridNhanh6_ThongTinNV.Visibility = Visibility.Visible;
+        }
+
+        //Thông tin nhân viên khác chuyển sang lương nhân viên khác
+        private void Button_Click_31(object sender, RoutedEventArgs e)
+        {
+            gridNhanh6_LuongNV.Visibility = Visibility.Visible;
+            gridNhanh6_ThongTinNV.Visibility = Visibility.Hidden;
+
+            string connString = @"Data Source=.\sqlexpress;Initial Catalog=QuanLyNhanSu;Integrated Security=True;Encrypt=False";
+
+                
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                int maNV = int.Parse(buttonLuongNV.Tag.ToString());
+                string sql = "SELECT * FROM tb_BANGLUONG WHERE MANV = @maNV";
+                using (SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    command.Parameters.AddWithValue("@maNV", maNV);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                txtBLCBO.Text = reader.GetInt64(1).ToString("N0") + " Đồng";
+                                txtBPCO.Text = reader.GetInt64(2).ToString("N0") + " Đồng";
+                                long tong = reader.GetInt64(1) + reader.GetInt64(2);
+                                txtBTNO.Text = tong.ToString("N0") + " Đồng";
+                            }
+                        }
+                        else
+                        {
+                            txtBLCBO.Text = "Chưa phân lương";
+                            txtBPCO.Text = "Chưa phân lương";
+                            txtBTNO.Text = "Chưa phân lương";
+                        }
+                    }
+                }
+                conn.Close();
+            }
+        }
+
+        // xử lý nhập text
+        private void txtBETENNVADD_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Focus_Text1(object sender, RoutedEventArgs e)
+        {
+            if(txtBETENNVADD.Text == "Vui lòng nhập thông tin")
+            {
+                txtBETENNVADD.Text = "";
+            }
+        }
+
+        private void Lost_Focus_Text1(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBETENNVADD.Text))
+            {
+                txtBETENNVADD.Text = "Vui lòng nhập thông tin";
+            }
+
+        }
+
+        private void Focus_Text2(object sender, RoutedEventArgs e)
+        {
+
+            if (txtBESDTADD.Text == "Vui lòng nhập thông tin")
+            {
+                txtBESDTADD.Text = "";
+            }
+
+        }
+
+        private void Lost_Focus_Text2(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBESDTADD.Text))
+            {
+                txtBESDTADD.Text = "Vui lòng nhập thông tin";
+            }
+
+        }
+        private void Focus_Text3(object sender, RoutedEventArgs e)
+        {
+
+            if (txtBEDCADD.Text == "Vui lòng nhập thông tin")
+            {
+                txtBEDCADD.Text = "";
+            }
+        }
+
+        private void Lost_Focus_Text3(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBEDCADD.Text))
+            {
+                txtBEDCADD.Text = "Vui lòng nhập thông tin";
+            }
+        }
+
+        private void Focus_Text4(object sender, RoutedEventArgs e)
+        {
+
+            if (txtADDCV.Text == "Vui lòng nhập thông tin")
+            {
+                txtADDCV.Text = "";
+            }
+        }
+
+        private void Lost_Focus_Text4(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtADDCV.Text))
+            {
+                txtADDCV.Text = "Vui lòng nhập thông tin";
+            }
+        }
+        private void Focus_Text5(object sender, RoutedEventArgs e)
+        {
+
+            if (txtUPDATECV.Text == "Vui lòng nhập thông tin")
+            {
+                txtUPDATECV.Text = "";
+            }
+        }
+
+        private void Lost_Focus_Text5(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUPDATECV.Text))
+            {
+                txtUPDATECV.Text = "Vui lòng nhập thông tin";
+            }
+        }
+        private void Focus_Text6(object sender, RoutedEventArgs e)
+        {
+
+            if (txtADDPB.Text == "Vui lòng nhập thông tin")
+            {
+                txtADDPB.Text = "";
+            }
+        }
+
+        private void Lost_Focus_Text6(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtADDPB.Text))
+            {
+                txtADDPB.Text = "Vui lòng nhập thông tin";
+            }
+        }
+        private void Focus_Text7(object sender, RoutedEventArgs e)
+        {
+
+            if (txtUPDATEPB.Text == "Vui lòng nhập thông tin")
+            {
+                txtUPDATEPB.Text = "";
+            }
+        }
+
+        private void Lost_Focus_Text7(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUPDATEPB.Text))
+            {
+                txtUPDATEPB.Text = "Vui lòng nhập thông tin";
+            }
+        }
+
+        private void Focus_Text8(object sender, RoutedEventArgs e)
+        {
+            string connString = @"Data Source=.\sqlexpress;Initial Catalog=QuanLyNhanSu;Integrated Security=True;Encrypt=False";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                int maNV = int.Parse(buttonLuongNV.Tag.ToString());
+                string sql = "SELECT * FROM tb_BANGLUONG WHERE MANV = @maNV";
+                using (SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    command.Parameters.AddWithValue("@maNV", maNV);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            if (txtBLCBOS.Text == reader.GetInt64(1).ToString())
+                            {
+                                txtBLCBOS.Text = "";
+                            }
+                        }
+                    }
+                }
+                conn.Close();
+            }
+        }
+
+        private void Lost_Focus_Text8(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBLCBOS.Text))
+            {
+                txtBLCBOS.Text = "Vui lòng nhập thông tin";
+            }
+        }
+
+        private void Focus_Text9(object sender, RoutedEventArgs e)
+        {
+            string connString = @"Data Source=.\sqlexpress;Initial Catalog=QuanLyNhanSu;Integrated Security=True;Encrypt=False";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                int maNV = int.Parse(buttonLuongNV.Tag.ToString());
+                string sql = "SELECT * FROM tb_BANGLUONG WHERE MANV = @maNV";
+                using (SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    command.Parameters.AddWithValue("@maNV", maNV);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            if (txtBPCOS.Text == reader.GetInt64(2).ToString())
+                            {
+                                txtBPCOS.Text = "";
+                            }
+                        }
+                    }
+                }
+                conn.Close();
+            }
+        }
+
+        private void Lost_Focus_Text9(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBPCOS.Text))
+            {
+                txtBPCOS.Text = "Vui lòng nhập thông tin";
+            }
+        }
+
+        // hiệu ứng nút
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            button.BorderBrush = new SolidColorBrush(Colors.Green);
+            button.Background = new SolidColorBrush(Colors.Green);
+            button.BorderThickness = new Thickness(2);
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            button.BorderBrush = null;
+            button.Background = null; 
+        }
+
+        // hủy sửa lương
+        private void Button_Click_32(object sender, RoutedEventArgs e)
+        {
+            gridNhanh6_SuaLuongNV.Visibility = Visibility.Hidden;
+            gridNhanh6_LuongNV.Visibility = Visibility.Visible;
+        }
+
+        // button Xong sửa lương
+        private void Button_Click_33(object sender, RoutedEventArgs e)
+        {
+            string connString = @"Data Source=.\sqlexpress;Initial Catalog=QuanLyNhanSu;Integrated Security=True;Encrypt=False";
+            int maNV = int.Parse(buttonLuongNV.Tag.ToString());
+
+
+            if (txtBLCBOS.Text == "Vui lòng nhập thông tin" || txtBPCOS.Text == "Vui lòng nhập thông tin" )
+            {
+                errorThieuThongTin.Visibility = Visibility.Visible;
+                return;
+            }
+            if (!int.TryParse(txtBLCBOS.Text, out _) || !int.TryParse(txtBPCOS.Text, out _))
+            {
+                errorSaiThongTin.Visibility = Visibility.Visible;
+                return;
+            }
+            gridNhanh6_SuaLuongNV.Visibility = Visibility.Hidden;
+            gridNhanh6_LuongNV.Visibility = Visibility.Visible;
+
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                string query = "UPDATE tb_BANGLUONG SET LCOBAN = @lCoBan, PHUCAP = @phuCap WHERE MANV = @maNV";
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    long lCoBan = long.Parse(txtBLCBOS.Text.Replace(".", ""));
+                    long phuCap = long.Parse(txtBPCOS.Text.Replace(".", ""));
+
+                    command.Parameters.AddWithValue("@maNV", maNV);
+                    command.Parameters.AddWithValue("@lCoBan", lCoBan);
+                    command.Parameters.AddWithValue("@phuCap", phuCap);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected == 0)
+                    {
+                        string insertSql = "INSERT INTO tb_BANGLUONG (LCOBAN, PHUCAP, MANV) VALUES (@lCoBan, @phuCap, @maNV)";
+                        using (SqlCommand insertCommand = new SqlCommand(insertSql, conn))
+                        {
+                            // Thay thế giá trị thực tế cho @luongCoBan, @phuCap, và @maNV
+                            insertCommand.Parameters.AddWithValue("@lCoBan", lCoBan);
+                            insertCommand.Parameters.AddWithValue("@phuCap", phuCap);
+                            insertCommand.Parameters.AddWithValue("@maNV", maNV);
+
+                            // Thực hiện thêm bản ghi
+                            insertCommand.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+
+            errorThieuThongTin.Visibility = Visibility.Hidden;
+            errorSaiThongTin.Visibility = Visibility.Hidden;
+            MessageBox.Show("Cập nhật thành công !");
+           
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM tb_BANGLUONG WHERE MANV = @maNV";
+                using (SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    command.Parameters.AddWithValue("@maNV", maNV);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                txtBLCBO.Text = reader.GetInt64(1).ToString("N0") + " Đồng";
+                                txtBPCO.Text = reader.GetInt64(2).ToString("N0") + " Đồng";
+                                long tong = reader.GetInt64(1) + reader.GetInt64(2);
+                                txtBTNO.Text = tong.ToString("N0") + " Đồng";
+                            }
+                        }
+                    }
+                }
+                conn.Close();
+            }
+        }
+
+        // chỗ sửa lương
+        private void Button_Click_34(object sender, RoutedEventArgs e)
+        {
+            gridNhanh6_SuaLuongNV.Visibility = Visibility.Visible;
+            gridNhanh6_LuongNV.Visibility = Visibility.Hidden;
+            errorThieuThongTin.Visibility = Visibility.Hidden;
+            errorSaiThongTin.Visibility = Visibility.Hidden;
+
+            string connString = @"Data Source=.\sqlexpress;Initial Catalog=QuanLyNhanSu;Integrated Security=True;Encrypt=False";
+
+                
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                int maNV = int.Parse(buttonLuongNV.Tag.ToString());
+                string sql = "SELECT * FROM tb_BANGLUONG WHERE MANV = @maNV";
+                using (SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    command.Parameters.AddWithValue("@maNV", maNV);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                txtBLCBOS.Text = reader.GetInt64(1).ToString();
+                                txtBPCOS.Text = reader.GetInt64(2).ToString();
+                            }
+                        }
+                        else
+                        {
+                            txtBLCBOS.Text = "Chưa phân lương";
+                            txtBPCOS.Text = "Chưa phân lương";
+                        }
+                    }
+                }
+                conn.Close();
+            }
         }
     }
 }
